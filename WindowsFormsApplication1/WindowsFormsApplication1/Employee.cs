61904bc24eca5c4bbce1560a47adccad3e499d34
madan <madan.mohan@quovantis.com>
2016-10-27 16:21:19 +0530

    first sample report created to demponstrate the Recursive Hierarchy

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace RecursiveHierarchyGroup
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int? ManagerId { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
